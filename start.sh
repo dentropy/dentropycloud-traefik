0be3df6e42c059b151b6b241f74922b2af46c545
root <root@localhost.localdomain>
2021-09-23 02:21:11 +0000

    Upgraded start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-startApps=("miniflux" "misskey" "monica" "qbitttorrent" "static" "syncthing" "trilium" "wikijs")
+
+# App List: "audioserve" "ELK" "filebrowser" "jellyfin" "misskey" "monica" "qbittorrent" "static" "syncthing" "trilium" "whoami-test"
+startApps=("miniflux" "misskey" "trilium" "wikijs")
+
+cd apps
 for value in ${!startApps[@]}
 do
     echo ${startApps[$value]}
     cd ${startApps[$value]} && ./start.sh
     cd ..
 done
-cd traefik/setup-authelia-secure
+cd ..
+# traefik options: authelia, authelia-secure, secure
+taefik="authelia-secure"
+# Networking apps: wireguard
+cd networking/traefik/${authelia-secure}
 ./start.sh
+cd ../..
+cd wireguard
+./sart.sh
