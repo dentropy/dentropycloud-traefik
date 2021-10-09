1ca86e8cf31238cb0bc4573ff8210a9ed21dc84a
Paul Mullins <personinternet@protonmail.com>
2021-10-09 15:16:24 +0000

    Please work now with the correct .env file

--- apps/ELK/start.sh
+++ apps/ELK/start.sh
@@ -3,18 +3,18 @@
 if [ -d "./docker-elk" ] 
 then
     echo "Starting docker-elk" 
-    cat ../.env | grep ELASTIC_PASSWORD
-    cd docker-elk && docker-compose --env-file ../../../.env up -d
+    cat ../../.env | grep ELASTIC_PASSWORD
+    cd docker-elk && docker-compose --env-file ../../.env up -d
 else
     echo "git cloning docker-elk"
     git clone https://github.com/deviantony/docker-elk.git
     docker volume create elasticsearch
     cp docker-compose.yml docker-elk/docker-compose.yml
-    if grep -q ELASTIC_PASSWORD ../.env; then
-        cat ../.env | grep ELASTIC_PASSWORD
+    if grep -q ELASTIC_PASSWORD ../../.env; then
+        cat ../../.env | grep ELASTIC_PASSWORD
     else
         tmpPASSWORD=`tr -cd '[:alnum:]' < /dev/urandom | fold -w31 | head -n1`
-        echo "ELASTIC_PASSWORD=$tmpPASSWORD" >> ../.env
+        echo "ELASTIC_PASSWORD=$tmpPASSWORD" >> ../../.env
         # ./docker-elk/kibana/config/kibana.yml
         lineNum=`grep -n elasticsearch.password: ./docker-elk/kibana/config/kibana.yml | cut -d : -f 1`
         sed -i "${lineNum}s/.*/elasticsearch.password: ${tmpPASSWORD}/g" ./docker-elk/kibana/config/kibana.yml
@@ -26,7 +26,7 @@ else
         sed -i "${lineNum}s/.*/        password => \"${tmpPASSWORD}\"/g" ./docker-elk/logstash/pipeline/logstash.conf 
     fi
     if grep -q ELK_VERSION ../../.env; then
-        cat ../.env | grep ELK_VERSION
+        cat ../../.env | grep ELK_VERSION
     else
         echo "ELK_VERSION=7.13.2" >> ../../.env
     fi
